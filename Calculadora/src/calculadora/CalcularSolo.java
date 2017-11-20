0dd612ad946d79c939b5139c4121b91780234fb7
lucas <lucas@Lucas>
2017-11-20 19:48:46 -0200

    Calculo de Base

--- Calculadora/src/calculadora/CalcularSolo.java
+++ Calculadora/src/calculadora/CalcularSolo.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora;
 
-/**
- *
- * @author lucas
- */
+
+//A soma de bases (SB) nos indica o número de cargas negativas dos coloides do solo que está ocupado pelos cátions básicos trocáveis.
+// A soma de bases é utilizado no cálculo das CTC's efetiva 
+//e a pH 7 ou potencial, no cálculo da percentagem de saturação por bases (V%) ]
+//e no cálculo da percentagem de saturação por Al³+.
 public class CalcularSolo {
     
+    private float SomaBase; 
+   
+    
+    //Metodo Construtor
+    public CalcularSolo()
+{
+        SomaBase = 0;
 }
+    
+    
+    public void setsomaBase(float somaBase)
+    {
+        this.SomaBase = SomaBase;
+    }
+    
+    public float getSomaBase()
+    {
+       return SomaBase;
+    }
+    
+    //SB = K + Ca + Mg + (Na)
+    public float calculo(float SomaBase,float potassio, float calcio, float magnesio, float sodio)
+    {
+        SomaBase = (potassio + calcio + magnesio + sodio);
+        return SomaBase;
+    }
+    
+    
+    
+}
+    
