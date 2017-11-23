ef88307fa8cba1e0ca48442de0a4293629fe4274
bruno <bruno@192.168.1.9>
2017-11-23 00:28:08 -0200

    Feito teste de soma do abubagem simples.amanha vou faz restos doas teste!

---
+++ Calculadora/test/AdubagemSimplesTest.java
@@ -0,0 +1,33 @@
+import calculadora.AdubagemSimples;
+import calculadora.AdubasSimplesJFrame;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author BMorais
+ */
+public class AdubagemSimplesTest {
+    
+    @Test
+    public void SomaPrevisto(){
+    
+        float previsto = 300;
+        float resultado;
+        AdubagemSimples calculo = new AdubagemSimples();
+        resultado = calculo.calculo(60, 20);
+        assertEquals(previsto, resultado, 0.001);
+    }
+    @Test
+    public void SomaPrevisto2(){
+    
+        float previsto = 200;
+        float resultado;
+        AdubagemSimples calculo = new AdubagemSimples();
+        resultado = calculo.calculo(60, 30);
+        assertEquals(previsto, resultado, 0.001);
+    }
+    
+    
+}
